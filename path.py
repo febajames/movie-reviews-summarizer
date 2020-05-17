1bbbabe91252027b882435c8444e026282e42e46
febajames <feba.james@ymail.com>
2020-05-17 18:32:55 +0800

    Project undertaken as part of text analytics and applications course

--- path.py
+++
@@ -1,7 +0,0 @@
-import sys
-import os
-
-sys.path.append(os.path.join(os.path.dirname(__file__), '../../'))
-
-ABS_PATH = os.path.abspath(os.path.dirname(sys.argv[0]))
-CONFIG_PATH = os.path.join(ABS_PATH, './config.json')  #replace './config.json' with your filename
